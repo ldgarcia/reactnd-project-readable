e535d9fb5a94e0e5b867450ae65319316fe83a10
Luis Diego García <ldgarcia27@gmail.com>
2017-09-15 03:35:39 -0600

    refactor: Add styles to sort form and widgets

--- frontend/src/components/SortForm.js
+++ frontend/src/components/SortForm.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SortForm = ({ form, handleInputChange }) => {
   return (
-    <form>
+    <form className="form-inline">
       Sort by:&nbsp;
-      <select name="by" value={ form.by } onChange={ handleInputChange }>
+      <select className="form-control" name="by" value={ form.by } onChange={ handleInputChange }>
         <option value="SORT_BY_SCORE">Score</option>
         <option value="SORT_BY_DATE">Date</option>
       </select>&nbsp;
-      <select name="order" value={ form.order } onChange={ handleInputChange }>
+      <select className="form-control" name="order" value={ form.order } onChange={ handleInputChange }>
         <option value="ASC">Ascending</option>
         <option value="DESC">Descending</option>
       </select>
