c0ee3e8f94fbf28c19ea797bd3889e906d5fa984
Luis Diego García <ldgarcia27@gmail.com>
2017-09-23 19:55:26 -0600

    refactor: Add PageNotFound component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
+import PageNotFound from './PageNotFound'
 import CategoryList from '../containers/CategoryList'
 import PostList from '../containers/PostList'
 import Post from '../containers/Post'
@@ -9,16 +10,29 @@ import EditPost from '../containers/EditPost'
 import AddComment from '../containers/AddComment'
 import EditComment from '../containers/EditComment'
 
-const App = () => (
-  <div className="container">
-    <CategoryList />
-    <Route exact path="/:category?/" component={ PostList } />
-    <Route exact path="/:category/:postId/" component={ Post } />
-    <AddPost />
-    <EditPost />
-    <AddComment />
-    <EditComment />
-  </div>
-)
+function App() {
+  return (
+    <div className="container">
+      <CategoryList />
+      <Switch>
+        <Route
+          exact
+          path="/:category/:postId/"
+          component={Post}
+        />
+        <Route
+          exact
+          path="/:category?/"
+          component={PostList}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
+      <AddPost />
+      <EditPost />
+      <AddComment />
+      <EditComment />
+    </div>
+  )
+}
 
 export default App;
